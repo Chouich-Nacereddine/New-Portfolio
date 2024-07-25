2ab6367288c4015278cb7d89d19aac04aa0d5715
CHOUICH <nacer-eddine.chouich@capgemini.com>
2024-07-25 14:16:13 +0100

    3rd commit

--- app/components/Certifications.jsx
+++ app/components/Certifications.jsx
@@ -1,26 +1,26 @@
-"use client"
-
 import React, { useEffect } from "react";
 import MarqueeDemo from "./MarqueeDemo";
 import ScrollReveal from 'scrollreveal';
 
 const Certifications = () => {
   useEffect(() => {
-    const sr = ScrollReveal({
-      distance: '50px',
-      duration: 1000,
-      easing: 'ease-in-out',
-      reset: true,
-    });
+    if (typeof window !== 'undefined') {
+      const sr = ScrollReveal({
+        distance: '50px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        reset: true,
+      });
 
-    sr.reveal('.reveal-top', { origin: 'top' });
-    sr.reveal('.reveal-bottom', { origin: 'bottom' });
+      sr.reveal('.reveal-top', { origin: 'top' });
+      sr.reveal('.reveal-bottom', { origin: 'bottom' });
+    }
   }, []);
 
   return (
-    <section className="w-[full] overflow-hidden px-8">
+    <section className="w-full overflow-hidden px-8">
       <div className="bg-[#d2e5ff] w-full h-[48vh] flex flex-col gap-3 justify-center items-center reveal-top">
-        <h1 className="py-2 text-3xl tracking-wider reveal-top">Certifications</h1>
+        <h1 className="py-2 text-3xl tracking-wider">Certifications</h1>
         <MarqueeDemo />
       </div>
     </section>
