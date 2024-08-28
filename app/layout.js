bcc116c991e5b73b3f0d98423e5001dc460dc449
unknown <nacreddinechouich0012@gmail.com>
2024-08-28 02:53:20 +0200

    7th commit --add Loading at start

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
     // Simulate loading time or wait until the app is fully loaded
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000); // Adjust this duration 
+    }, 1000); // Adjust this duration 
 
     return () => clearTimeout(timer);
   }, []);
